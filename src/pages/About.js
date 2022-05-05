850883bc4a85e640db3b2e5cd1872aa5beb27a30
sunil-dhi <sunildhimanmatharu@gmail.com>
2022-05-05 19:43:09 -0400

    all files are added

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/About.css'
+import MultiplePizzas from '../assets/multiplePizzas.jpeg'
+function About() {
+  return (
+    <div className='about'>
+        <div className="aboutTop" style={{backgroundImage: `url(${MultiplePizzas})`}}>  </div>
+
+        <div className="aboutBottom"> 
+        <h1>About us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut qui culpa accusantium error repellat in maxime vero nobis voluptatem molestias? Quas perspiciatis asperiores repellat inventore ipsam ex id, reiciendis saepe, ducimus doloribus repellendus necessitatibus soluta, omnis incidunt? Necessitatibus voluptatibus vel recusandae porro, exercitationem ab vitae fuga illum animi libero sint dolore facilis natus atque quibusdam architecto sed voluptatum id laborum molestiae! Reprehenderit nobis asperiores eum! Vitae asperiores numquam illo quia placeat facere labore unde ut rerum veniam! Voluptas, commodi? Blanditiis praesentium possimus corrupti illo nemo autem culpa itaque a quod quia similique natus laudantium, suscipit fugit vel necessitatibus. Libero veniam quo magni eius ex iure inventore voluptatibus, nostrum labore blanditiis consectetur repellendus. Tempora ad officia fugit iure harum odio ratione eveniet praesentium, quidem iste dignissimos ex, sunt eos accusantium a amet in nam cumque neque et doloribus. Voluptates perferendis odit natus aut quam ad maiores architecto et expedita ipsum ipsam cupiditate, porro rem optio minima facilis laborum esse doloremque beatae eaque magnam sunt atque magni at? Optio facere repudiandae ipsum quod alias quos deserunt expedita voluptatibus ipsa aut sequi at corrupti molestiae architecto voluptates quasi cum dolorum, sapiente unde voluptas natus? Delectus quibusdam veniam, cupiditate velit nihil tempora nemo laborum.</p>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Contact.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import leftpizza from '../assets/pizzaLeft.jpg'
+import '../styles/Contact.css'
+function Contact() {
+  return (
+    <div className='contact'>
+        <div className="leftSide" style={{backgroundImage: `url(${leftpizza})`}}></div>
+        <div className="rightSide">
+
+         <h1>Contact us</h1>
+         <form method='post' id="contact-form">
+             <label htmlFor="name" >Full name</label>
+             <input type="text" placeholder='Enter full name' name='name' />
+
+             <label htmlFor="email" >E-mail</label>
+             <input type="email" placeholder='Enter Email...' name='email' />
+
+             <label htmlFor="Phone" >Full name</label>
+             <input type="text" placeholder='Enter full name' name='name' />
+
+              <textarea name="message"  rows="6" placeholder='Enter message...' required></textarea>
+    <button type='submit'>Send message</button>
+            
+         </form>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
+
+
